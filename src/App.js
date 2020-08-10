30cbe13087697b09bce18ca219ba1d5b6f1d466e
Adredrs23 <adredars@gmail.com>
2020-08-10 18:47:36 +0530

    Fixed Browser Router basename property for github deployment

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-		<Router>
+		<Router basename={window.location.pathname || ''}>
 			<Switch>
 
 				<Route path="/" exact>
