f0233bc1d4b3e930ebf94ceb7c494144573e8af8
Adredrs23 <adredars@gmail.com>
2020-08-10 14:39:15 +0530

    Updates

---
+++ precache-manifest.aba32748c184312b70d60f632e2bb742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99617246b42ce7e08c9cd63449925931",
+    "url": "/React-Tinder-Clonish/index.html"
+  },
+  {
+    "revision": "ac6d7fc94a3b1e04e689",
+    "url": "/React-Tinder-Clonish/static/css/main.b1e8d74b.chunk.css"
+  },
+  {
+    "revision": "57912268df4625452f43",
+    "url": "/React-Tinder-Clonish/static/js/2.c988b930.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/React-Tinder-Clonish/static/js/2.c988b930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6d7fc94a3b1e04e689",
+    "url": "/React-Tinder-Clonish/static/js/main.e2a9a9d5.chunk.js"
+  },
+  {
+    "revision": "6f2edc9b09e4e1f8eac7",
+    "url": "/React-Tinder-Clonish/static/js/runtime-main.a353f091.js"
+  }
+]);
